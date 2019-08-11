04520e8b09ec6481625c0c64bba1ce0149cc6fe4
Christopher Osthues <osthues.christopher@web.de>
2019-08-11 17:13:33 +0200

    Init structure for confluence

---
+++ confluence-rest-api-client/src/main/java/org/elegycode/confluence/rest/api/client/App.java
@@ -0,0 +1,13 @@
+package org.elegycode.confluence.rest.api.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
